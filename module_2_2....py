c7fbe8a34e47e058aa2cd0592947ae0ef1235ac0
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-11 21:35:59 +0300

    module_2_2....py
    Операторы if, elif, else."

---
+++ module_2_2....py
@@ -0,0 +1,12 @@
+# first, second и third
+# print('введите данные first:')
+first = (int(input('веденное сисло first =', )))
+second = (int(input('веденное сисло second =', )))
+third = (int(input('веденное сисло third =', )))
+if first == second and first == third and second == third:
+    print(3 )
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+# незнаю как лучше написать, но я художник. Я так вижу.
